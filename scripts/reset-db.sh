7cf1510b2bafb98fabebb10ac939953fc77215a4
Ben Nava <arielnmz@yandex.com>
2024-08-17 22:03:24 -0600

    Fixed script to reset the DB

--- scripts/reset-db.sh
+++ scripts/reset-db.sh
@@ -12,9 +12,14 @@ sleep 5s
 
 echo "Seeding DynamoDB..."
 
+if [ -z "$DYNAMODB_ENDPOINT" ]; then
+  echo "WARN: The env var \$DYNAMODB_ENDPOINT is not set, this script will use the DEFAULT endpoint: http://localhost:8000"
+  export DYNAMODB_ENDPOINT="http://localhost:8000"
+fi
+
 pushd "api"
   . .venv/bin/activate &&
-  python -c 'from mini_kanban_api import seed; seed.create_table()';
+    python -c 'from mini_kanban_api import seed; seed.create_table()';
 popd
 
 echo "Done"
